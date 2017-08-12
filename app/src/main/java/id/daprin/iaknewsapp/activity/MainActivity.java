8b6290d6e043f1c30c04e302e549ec9f0df37910
Diani Opiari <dianiopiari89@gmail.com>
2017-08-12 14:47:14 +0800

    coomit

--- app/src/main/java/id/daprin/iaknewsapp/activity/MainActivity.java
+++ app/src/main/java/id/daprin/iaknewsapp/activity/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     LinearLayoutManager mLinearLayoutManager;
     NewsAdapter mAdapter;
-
+    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
